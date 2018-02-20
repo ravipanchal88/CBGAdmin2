afd6f15f685207e96541b8f3230e53b39b4926c9
Ravi Panchal <pancha.ravi@gmail.com>
2018-02-19 19:59:14 -0500

    updating the app js to read static files from public directory

--- app.js
+++ app.js
@@ -43,7 +43,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+//app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(__dirname + '/public'));
+
 //Middleware for Passport
 app.use(session({secret: 'wwerwjflwnfkjhwjkrhewjkrhewjkrhjw12'}));
 app.use(passport.initialize());
